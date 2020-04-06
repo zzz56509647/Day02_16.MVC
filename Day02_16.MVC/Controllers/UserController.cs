a342372880b80f120fbb2af75bfce889805f142c
张凯 <@ZK>
2020-04-06 09:59:28 +0800

    添加项目文件。

---
+++ Day02_16.MVC/Controllers/UserController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+
+namespace Day02_16.MVC.Controllers
+{
+    public class UserController : Controller
+    {
+        HttpClientHelper hc = new HttpClientHelper("http://localhost:59001/api/");
+        public ActionResult Show()
+        {
+            BandSel();
+            var list = JsonConvert.DeserializeObject<List<UserInfo>>(hc.Get("User"));
+            return View(list);
+        }
+        [HttpPost]
+        public ActionResult Show(string name, string did)
+        {
+            BandSel();
+            var list = JsonConvert.DeserializeObject<List<UserInfo>>(hc.Get("User"));
+            if (!string.IsNullOrEmpty(name))
+            {
+                list = list.Where(s => s.Uname.Contains(name)).ToList();
+            }
+            if (!string.IsNullOrEmpty(did))
+            {
+                list = list.Where(s => s.DetpId == int.Parse(did)).ToList();
+            }
+            return View(list);
+        }
+
+        public void BandSel()
+        {
+            var list = JsonConvert.DeserializeObject<List<DetpInfo>>(hc.Get("Detp"));
+            ViewBag.bandsel = new SelectList(list, "Did", "Dname");
+        }
+
+        public ActionResult Add()
+        {
+            BandSel();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(UserInfo u)
+        {
+            if (hc.Post("User", JsonConvert.SerializeObject(u)) == "1")
+            {
+                return Content("<script>alert('添加成功');location.href='/User/Show';</script>");
+            }
+            else
+            {
+                return Content("<script>alert('添加失败')</script>");
+            }
+        }
+
+        public ActionResult Del(string id)
+        {
+            if (hc.Delete("User/" + id) == "1")
+            {
+                return Content("<script>alert('删除成功');location.href='/User/Show';</script>");
+            }
+            else
+            {
+                return Content("<script>alert('删除失败');location.href='/User/Show';</script>");
+
+            }
+
+        }
+
+        public ActionResult Upt(int id)
+        {
+            BandSel();
+            var list = JsonConvert.DeserializeObject<UserInfo>(hc.Get("User/" + id));
+            return View(list);
+        }
+        [HttpPost]
+        public ActionResult Upt(UserInfo u)
+        {
+            if (hc.Put("User", JsonConvert.SerializeObject(u)) == "1")
+            {
+                return Content("<script>alert('修改成功');location.href='/User/Show';</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('修改失败')</script>");
+            }
+            return View(u);
+        }
+    }
+}
---
+++ Day02_16.MVC/HttpClientHelper.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Day02_16.MVC
+{
+    public class HttpClientHelper
+    {
+        public HttpClientHelper(string baseAddr)
+        {
+            this.BaseAddr = baseAddr;
+        }
+        public  string BaseAddr { get; set; }
+
+        public  string Get(string Url)
+        {
+            HttpClient client = new HttpClient();
+            //设置 API的 基地址
+            client.BaseAddress = new Uri(BaseAddr);
+            //设置 默认请求头ACCEPT
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //发送GET请求
+            HttpResponseMessage msg =  client.GetAsync(Url).Result;
+            //判断结果是否成功
+            if (msg.IsSuccessStatusCode)
+            {
+                //返回响应结果
+                return msg.Content.ReadAsStringAsync().Result;
+            }
+            //返回空字符串，表示响应错误
+            return "";
+        }
+
+        public  string Delete(string Url)
+        {
+            HttpClient client = new HttpClient();
+            //设置 API的 基地址
+            client.BaseAddress = new Uri(BaseAddr);
+            //设置 默认请求头ACCEPT
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //发送GET请求
+            HttpResponseMessage msg = client.DeleteAsync(Url).Result;
+            //判断结果是否成功
+            if (msg.IsSuccessStatusCode)
+            {
+                //返回响应结果
+                return msg.Content.ReadAsStringAsync().Result;
+            }
+            //返回空字符串，表示响应错误
+            return "";
+        }
+
+        public  string Post(string Url,string JsonData)
+        {
+            HttpClient client = new HttpClient();
+            //设置 API的 基地址
+            client.BaseAddress = new Uri(BaseAddr);
+            //设置 默认请求头ACCEPT
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            //设置消息体
+            HttpContent content = new StringContent(JsonData);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            //发送Post请求
+            HttpResponseMessage msg = client.PostAsync(Url,content).Result;
+            //判断结果是否成功
+            if (msg.IsSuccessStatusCode)
+            {
+                //返回响应结果
+                return msg.Content.ReadAsStringAsync().Result;
+            }
+            //返回空字符串，表示响应错误
+            return "";
+        }
+
+        public  string Put(string Url, string JsonData)
+        {
+            HttpClient client = new HttpClient();
+            //设置 API的 基地址
+            client.BaseAddress = new Uri(BaseAddr);
+            //设置 默认请求头ACCEPT
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            //设置消息体
+            HttpContent content = new StringContent(JsonData);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            //发送Post请求
+            HttpResponseMessage msg = client.PutAsync(Url, content).Result;
+            //判断结果是否成功
+            if (msg.IsSuccessStatusCode)
+            {
+                //返回响应结果
+                return msg.Content.ReadAsStringAsync().Result;
+            }
+            //返回空字符串，表示响应错误
+            return "";
+        }
+
+
+    }
+}
